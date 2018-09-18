8d4f4977a077503b5fbf2108ede353426da965b8
Mark SUKHAREV <msukhare@e3r13p13.42.fr>
2018-09-18 16:51:25 +0200

    debut de la class cnn, update train_cnn.py et rajout du test_set

--- train_cnn.py
+++ train_cnn.py
@@ -6,7 +6,7 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/09/17 16:28:47 by msukhare          #+#    #+#              #
-#    Updated: 2018/09/17 17:01:58 by msukhare         ###   ########.fr        #
+#    Updated: 2018/09/18 16:49:45 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,10 +14,11 @@ import numpy as np
 import struct as st
 import sys
 from matplotlib import pyplot as plt
+import tensorflow as tf
 
-def read_file_idx():
-    image_file = open("train-images-idx3-ubyte", "rb")
-    label_file = open("train-labels-idx1-ubyte", "rb")
+def read_file_idx(images, labels):
+    image_file = open(images, "rb")
+    label_file = open(labels, "rb")
     magic_image, nb_image, rows, cols = st.unpack('>IIII',image_file.read(16))
     magic_label, nb_labels = st.unpack('>II', label_file.read(8))
     Y = np.asarray(st.unpack('>'+'B'*nb_labels, label_file.read(nb_labels))).reshape(nb_labels, 1)
@@ -26,11 +27,16 @@ def read_file_idx():
             rows, cols))
     return (X, Y, nb_image, rows, cols)
 
-
 def main():
-    X, Y, m, rows, cols = read_file_idx()
-    plt.imshow(X[45], interpolation='none')
-    plt.show()
+    X_train, Y_train, m, rows, cols = read_file_idx("train-images-idx3-ubyte", "train-labels-idx1-ubyte")
+    X_test, Y_test, m, rows, cols = read_file_idx("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte")
+    #plt.imshow(X[45], interpolation='none', cmap='gray')
+    #plt.show()
+    X_train = X_train / 255
+    Y_train = Y_train / 255
+    X_test = X_test / 255
+    Y_test = Y_test / 255
+
 
 
 if __name__ == "__main__":
