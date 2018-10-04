7c0581411e9ce3bc26314483496e7d36f600ec94
Mark SUKHAREV <msukhare@e3r7p12.42.fr>
2018-10-04 17:12:44 +0200

    ajout du fichier cnn_architecture qui contient l'architecture de lenet5

---
+++ cnn_architecture.py
@@ -0,0 +1,52 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    cnn_architecture.py                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/10/04 09:42:59 by msukhare          #+#    #+#              #
+#    Updated: 2018/10/04 17:11:31 by msukhare         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import tensorflow as tf
+
+def define_dense_layers(in_put, nb_before, nb_neurones):
+    weights = tf.Variable(tf.truncated_normal([nb_before, nb_neurones],\
+            stddev=0.05, seed=5), name="weights1")
+    bias = tf.Variable(tf.truncated_normal([1, nb_neurones], stddev=0.05, seed=5),\
+            name="bias1")
+    return (tf.nn.relu(tf.matmul(in_put, weights) + bias))
+
+def create_conv_layer(input, filter, p, s, bias, name, act_funct):
+    weights = tf.Variable(tf.truncated_normal(filter, stddev=0.05, seed=5),\
+            name=name+"W")
+    bias = tf.Variable(tf.truncated_normal(bias, stddev=0.05, seed=5),\
+            name=name+"B")
+    conv_layer = tf.nn.conv2d(input, weights, s, padding=p)
+    conv_layer += bias
+    if (act_funct == 1):
+        conv_layer = tf.nn.relu(conv_layer)
+    return (conv_layer)
+
+def define_conv_for_net5(x):
+    layer1 = create_conv_layer(x, [5, 5, 1, 6], "VALID", [1, 1, 1, 1],\
+            [1, 1, 6], "first_conv_layer", 0)
+    layer1 = tf.nn.pool(layer1, [2, 2], pooling_type="MAX",\
+            padding="VALID", strides=[2, 2])
+    layer2 = create_conv_layer(layer1, [5, 5, 6, 16], "VALID", [1, 1, 1, 1],\
+            [1, 1, 16], "sec_conv_layer", 0)
+    layer2 = tf.nn.pool(layer2, [2, 2], pooling_type="MAX",\
+            padding="VALID", strides=[2, 2])
+    return (layer2)
+
+def init_net5(x, y):
+    out_conv = define_conv_for_net5(x)
+    flatten_layer = tf.reshape(out_conv, [-1, (out_conv.shape[1] *\
+            out_conv.shape[2] * out_conv.shape[3])])
+    out_dense = define_dense_layers(flatten_layer, 256, 120)
+    out_dense = define_dense_layers(out_dense, 120, 84)
+    out_dense = define_dense_layers(out_dense, 84, 10)
+    return (tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(\
+            logits=out_dense, labels=y)), tf.nn.softmax(out_dense))
--- train_cnn.py
+++ train_cnn.py
@@ -6,7 +6,7 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/09/17 16:28:47 by msukhare          #+#    #+#              #
-#    Updated: 2018/10/03 17:12:37 by msukhare         ###   ########.fr        #
+#    Updated: 2018/10/04 17:11:29 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,8 +17,8 @@ import sys
 from matplotlib import pyplot as plt
 import tensorflow as tf
 from random import *
-from numpy.linalg import inv
 import scipy as sc
+from cnn_architecture import init_net5
 
 def read_file_idx(images, labels):
     try:
@@ -36,25 +36,8 @@ def read_file_idx(images, labels):
     Y = np.asarray(st.unpack('>'+'B'*nb_labels, label_file.read(nb_labels))).reshape(nb_labels, 1)
     byte_read = (rows * cols * nb_image * 1)
     X = np.asarray(st.unpack('>'+'B'*byte_read, image_file.read(byte_read)),\
-            dtype=np.float32).reshape((nb_image, 1, rows, cols))
+            dtype=np.float32).reshape((nb_image, 1, rows, cols, 1))
     return (X, Y, nb_image, rows, cols)
-
-def create_conv_layer(input, filter, p, s, bias, name):
-    weights = tf.Variable(tf.truncated_normal(filter, stddev=0.05, seed=5),\
-            name=name+"W")
-    bias = tf.Variable(tf.truncated_normal(bias, stddev=0.05, seed=5),\
-            name=name+"B")
-    conv_layer = tf.nn.conv2d(input, weights, s, padding=p)
-    conv_layer += bias
-    #conv_layer = tf.nn.relu(conv_layer)
-    return (conv_layer)
-
-def define_dense_layers(in_put, nb_before, nb_neurones):
-    weights = tf.Variable(tf.truncated_normal([nb_before, nb_neurones],\
-            stddev=0.05, seed=5), name="weights1")
-    bias = tf.Variable(tf.truncated_normal([1, nb_neurones], stddev=0.05, seed=5),\
-            name="bias1")
-    return (tf.nn.relu(tf.matmul(in_put, weights) + bias))
 
 def get_new_y(y, batch, nb_class):
     ret = np.zeros((batch, nb_class), dtype=int)
@@ -62,63 +45,30 @@ def get_new_y(y, batch, nb_class):
         ret[i][y[i]] = 1
     return (ret)
 
-def define_conv_for_net5(x, y):
-    layer1 = create_conv_layer(x, [5, 5, 1, 6], "VALID", [1, 1, 1, 1],\
-            [1, 1, 6], "first_conv_layer")
-    layer1 = tf.nn.pool(layer1, [2, 2], pooling_type="MAX",\
-            padding="VALID", strides=[2, 2])
-    layer2 = create_conv_layer(layer1, [5, 5, 6, 16], "VALID", [1, 1, 1, 1],\
-            [1, 1, 16], "sec_conv_layer")
-    layer2 = tf.nn.pool(layer2, [2, 2], pooling_type="MAX",\
-            padding="VALID", strides=[2, 2])
-    return (layer2)
+def show_graph(nb_epoch, train, cost):
+    x_smooth = np.linspace(nb_epoch.min(), nb_epoch.max(), 300)
+    y_smooth = sc.interpolate.spline(nb_epoch, train, x_smooth)
+    y_smooth2 = sc.interpolate.spline(nb_epoch, cost, x_smooth)
+    plt.plot(x_smooth, y_smooth)
+    plt.plot(x_smooth, y_smooth2)
+    print("all ok")
+    plt.show()
 
-def init_net5(x, y):
-    out_conv = define_conv_for_net5(x, y)
-    flatten_layer = tf.reshape(out_conv, [-1, (out_conv.shape[1] *\
-            out_conv.shape[2] * out_conv.shape[3])])
-    out_dense = define_dense_layers(flatten_layer, 256, 120)
-    out_dense = define_dense_layers(out_dense, 120, 84)
-    out_dense = define_dense_layers(out_dense, 84, 10)
-    return (tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(\
-            logits=out_dense, labels=y)), tf.nn.softmax(out_dense))
-
-def spline(X, Y):
-    h = np.zeros(((len(X) - 1), 1), dtype=float)
-    for i in range((len(X) - 1)):
-        h[i][0] = X[i + 1] - X[i]
-    f = np.zeros((len(X), 1), dtype=float)
-    for i in range(1, (len(X) - 1)):
-        tmp = (Y[i + 1] - Y[i]) / h[i][0]
-        tmp1 = (Y[i] - Y[i - 1]) / h[i - 1]
-        f[i][0] = tmp - tmp1
-    r = np.zeros((len(X), len(X)), dtype=float)
-    r[0][0] = 1
-    r[(len(X) - 1)][(len(X) - 1)] = 1
-    for i in range(1, (len(X) - 1)):
-        r[i][i] = (h[i - 1][0] + h[i][0]) / 3
-    for i in range(1, (len(X) - 1)):
-        r[i][i + 1] = h[i][0] / 6
-        r[i][i - 1] = h[i][0] / 6
-    m = inv(r).dot(f)
-    c = np.zeros(((len(X) - 1), 1), dtype=float)
-    cprime = np.zeros(((len(X) - 1), 1), dtype=float)
-    for i in range((len(X) - 1)):
-        c[i][0] = (Y[i + 1] - Y[i]) / h[i][0] - h[i] / 6 * (m[i + 1][0] - m[i][0])
-        cprime[i][0] = Y[i] - m[i][0] * ((h[i][0]**2) / 6)
-    print(h, "\n\n", f, "\n\n", r, "\n\n", m, "\n\n", c, "\n\n", cprime)
-
-def good_pred(pred, y):
-    if (pred[0][y] == np.amax(pred)):
-        return (1)
-    return (0)
+def get_class(pred):
+    maxi = pred[0][0]
+    index = 0
+    for i in range(10):
+        if (pred[0][i] > maxi):
+            index = i
+            maxi = pred[0][i]
+    return (index)
 
 def train_modele(cross_entropy, out_put, X_train, Y_train, x, y, X_cost, Y_cost, X_test, Y_test, m):
     init = tf.global_variables_initializer()
     #learning_rate = tf.Variable(0.005, tf.float32)
     training = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-    epoch = 5000
-    batch = 32
+    epoch = 300
+    batch = 64
     nb_epoch = np.zeros((epoch), dtype=float)
     cost = np.zeros((epoch), dtype=float)
     train = np.zeros((epoch), dtype=float)
@@ -127,46 +77,39 @@ def train_modele(cross_entropy, out_put, X_train, Y_train, x, y, X_cost, Y_cost,
         for i in range(epoch):
             start = randint(0, (floor(m * 0.8) - batch))
             start2 = randint(0, (floor(m * 0.2) - batch))
-            start3 = randint(0, 9968)
             avg = 0
             avg_cost = 0
             accu = 0
             for j in range(batch):
-                X = np.reshape(X_train[(start + j)], (1, 28, 28, 1))
-                X_c = np.reshape(X_cost[(start2 + j)], (1, 28, 28, 1))
                 Y = get_new_y(Y_train[(start + j)], 1, 10)
                 Y_c = get_new_y(Y_cost[(start2 + j)], 1, 10)
                 _, c = sess.run([training, cross_entropy],\
-                        feed_dict={x: X, y: Y})
+                        feed_dict={x: X_train[(start + j)], y: Y})
                 avg += c
-                c  = sess.run(cross_entropy, feed_dict={x: X_c, y: Y_c})
+                c  = sess.run(cross_entropy, feed_dict={x: X_cost[(start2 + j)], y: Y_c})
                 avg_cost += c
             cost[i] = avg_cost / batch
             nb_epoch[i] = i
             train[i] = avg / batch
             print("epoch= ", i, "cost_train= ", train[i], "cost_test= ", cost[i])
-        nb_right = 0
+            if (train[i] <= 0.09):
+                break
+        good_pred = 0
         for i in range(10000):
-            X_t = np.reshape(X_test[i], (1, 28, 28, 1))
             Y_t = get_new_y(Y_test[i], 1, 10)
-            nb_right += good_pred(sess.run(out_put, feed_dict={x: X_t, y: Y_t}), Y_test[i])
+            if (get_class(sess.run(out_put, feed_dict={x: X_test[i], y: Y_t})) == Y_test[i]):
+                good_pred += 1
         sess.close()
-    print("accuracy = ", (nb_right / 10000))
-    x_smooth = np.linspace(nb_epoch.min(), nb_epoch.max(), (epoch / 2))
-    y_smooth = sc.interpolate.spline(nb_epoch, train, x_smooth)
-    y_smooth2 = sc.interpolate.spline(nb_epoch, cost, x_smooth)
-    plt.plot(x_smooth, y_smooth)
-    plt.plot(x_smooth, y_smooth2)
-    plt.show()
+    print("accuracy: ", good_pred / 10000)
+    show_graph(nb_epoch, train, cost)
 
 def main():
     if (len(sys.argv) < 4):
         print("need more file")
         sys.exit()
-   # spline([0.0, 0.5, 1.0, 1.5, 2.0], [0.0, 0.4794, 0.8415, 0.9975, 0.9093])
-   # sys.exit()
     X_train, Y_train, m, rows, cols = read_file_idx(sys.argv[1], sys.argv[2])
     X_test, Y_test, m_test, rows, cols = read_file_idx(sys.argv[3], sys.argv[4])
+    #show handwritten number
     #plt.imshow(X[45], interpolation='none', cmap='gray')
     #plt.show()
     X_train = X_train / 255
@@ -176,7 +119,7 @@ def main():
     x = tf.placeholder(tf.float32, shape=[1, 28, 28, 1])
     y = tf.placeholder(tf.float32, shape=[1, 10])
     cross_entropy, out_put = init_net5(x, y)
-    #graph = tf.get_default_graph()
+    #graph = tf.get_default_graph()  put graphe tensorflow
     #for op in graph.get_operations():
      #   print(op.name)
     train_modele(cross_entropy, out_put, X_train, Y_train, x, y, X_cost, Y_cost, X_test, Y_test, m)
